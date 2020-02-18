902f29ac33bafb8bb39bc441959db10dd76e5ab0
jassmanni <jassmanniq@gmail.com>
2020-02-18 11:16:27 -0400

    login and register basic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  include SessionsHelper
+
+  before_action :logged_in_user
+
+  def logged_in_user
+    if !logged_in?
+      flash[:danger] = I18n.t 'user.errors.no_user'
+      redirect_to login_path
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources 'users'
+  resources 'products'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  root 'products#index'
 end
